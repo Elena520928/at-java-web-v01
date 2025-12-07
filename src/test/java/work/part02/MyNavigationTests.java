78785278b29d6d74d00f1611f309644747f34b59
Petrova Elena <petrovaelena19871987@mail.ru>
2025-12-07 13:19:27 +0300

    MyNavigationTest

---
+++ src/test/java/work/part02/MyNavigationTests.java
@@ -0,0 +1,23 @@
+package work.part02;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class MyNavigationTests {
+    @Test
+    void testNavigation() {
+        open("https://ya.ru/");
+        sleep(10_000);
+        open("https://ru.wikipedia.org");
+        sleep(3_000);
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        sleep(3_000);
+        back();
+        sleep(3_000);
+        back();
+        sleep(3_000);
+        forward();
+        sleep(10_000);
+    }
+}
